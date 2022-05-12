b846f70c2b1b0ba07071cb21efbf75c1ed9766c6
Facuscar <facundo.scardillo@dracoder.com>
2022-05-12 10:48:52 -0300

    Add: routing

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { CountryComponent } from './pages/country/country.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
+
 
 
 
@@ -25,7 +27,8 @@ import { ByRegionComponent } from './pages/by-region/by-region.component';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class CountryModule { }
